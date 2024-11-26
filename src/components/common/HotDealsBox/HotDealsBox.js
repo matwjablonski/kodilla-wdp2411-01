e97e7c415f4ef8315bc5587c49a51836213d6b7f
Maks Birecki <maksymilian.birecki@gmail.com>
2024-11-26 16:14:00 +0100

    start of task

---
+++ src/components/common/HotDealsBox/HotDealsBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// import styles from './HotDealsBox.module.scss';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {
+//   faStar,
+//   faExchangeAlt,
+//   faShoppingBasket,
+// } from '@fortawesome/free-solid-svg-icons';
+// import { faHeart } from '@fortawesome/free-regular-svg-icons';
+// import Button from '../Button/Button';
+
+const HotDealsBox = ({}) => <div></div>;
+
+HotDealsBox.propTypes = {};
+
+export default HotDealsBox;
---
+++ src/components/features/PromotedProducts/PromotedProducts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styles from './ProductSearch.module.scss';
+
+const PromotedProducts = () => <div></div>;
+
+PromotedProducts.propTypes = {};
+
+export default PromotedProducts;
