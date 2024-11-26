583faeb0a236af44d137dbf8145d9db69aa7e592
Maks Birecki <maksymilian.birecki@gmail.com>
2024-11-26 22:17:16 +0100

    change the way how display on phone and tablet was written

--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -14,26 +14,26 @@ import FeatureBox from '../../common/FeatureBox/FeatureBox';
 const FeatureBoxes = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div className={styles.customRow}>
-        <a href='#' className='col'>
+      <div className='row'>
+        <a href='#' className='col col-lg-3 col-md-6 col-6'>
           <FeatureBox icon={faTruck}>
             <h5>Free shipping</h5>
             <p>All orders</p>
           </FeatureBox>
         </a>
-        <a href='#' className='col'>
+        <a href='#' className='col col-lg-3 col-md-6 col-6'>
           <FeatureBox icon={faHeadphones}>
             <h5>24/7 customer</h5>
             <p>support</p>
           </FeatureBox>
         </a>
-        <a href='#' className='col'>
+        <a href='#' className='col col-lg-3 col-md-6 col-6'>
           <FeatureBox icon={faReplyAll}>
             <h5>Money back</h5>
             <p>guarantee</p>
           </FeatureBox>
         </a>
-        <a href='#' className='col'>
+        <a href='#' className='col col-lg-3 col-md-6 col-6'>
           <FeatureBox icon={faBullhorn}>
             <h5>Member discount</h5>
             <p>First order</p>
