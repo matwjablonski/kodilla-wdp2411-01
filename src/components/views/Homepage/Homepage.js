643ded712bb871c721c23252bb942c995f1b076e
Paweł <p.grabczak@cognitionhub.com>
2024-11-29 12:02:21 +0100

    WDP241101-20

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,14 +5,19 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import PropTypes from 'prop-types';
 
-const Homepage = () => (
-  <div className={styles.root}>
-    <FeatureBoxes />
-    <NewFurniture />
-  </div>
-);
+const Homepage = mode => {
+  return (
+    <div className={styles.root}>
+      <FeatureBoxes />
+      <NewFurniture mode={mode} />
+    </div>
+  );
+};
 
-// Homepage.propTypes = {};
+Homepage.propTypes = {
+  mode: PropTypes.string,
+};
 
 export default Homepage;
